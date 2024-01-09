ab5b70d023af0f91bd213f7ca888b9c2185f43d2
Roddy-Rafael-Tejeda-Rosario-21-25 <128174391+Roddy-21-25@users.noreply.github.com>
2024-01-09 01:36:50 -0400

    first commit

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,11 @@
+// import { render, screen } from '@testing-library/react'
+// import Home from '@/app/page'
+
+// describe('Home', () => {
+//     it('should have Docs text', () => {
+//         render(<Home />)
+//         const myElem = screen.getByText('Docs')
+//         expect(myElem).toBeInTheDocument()
+//     })
+// })
+
